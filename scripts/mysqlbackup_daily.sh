35f1bb886057deea9a62c36f44d1c0becb6846f5
Nickster258 <nickster258@gmail.com>
2016-01-19 01:34:14 -0500

    Update mysqlbackup_daily.sh

--- scripts/mysqlbackup_daily.sh
+++ scripts/mysqlbackup_daily.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+now=`date +%Y-%m`
+db="DATABASESAVENAME"
+dbuser="MYSQLUSER"
+host="DATABASEHOST"
+pass="MYSQLPASSWORD"
+systemuser="SYSTEMUSER"
+mysqldump -u"$dbuser" -h"$host" -p"$pass" --hex-blob --routines --triggers --all-databases | gzip > /home/"$systemuser"/MySQLBackups/daily/"$db"_"$now".sql.gz
+find /home/mcadmin/MySQLBackups/daily/ -mindepth 1 -mtime +30 -delete
