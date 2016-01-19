58936d12dee88c10a537d537c87c72eda05ca2ec
Nickster258 <nickster258@gmail.com>
2016-01-19 01:33:26 -0500

    Create mysqlbackup_monthly.sh

---
+++ scripts/mysqlbackup_monthly.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+now=`date +%Y-%m`
+db="DATABASESAVENAME"
+dbuser="MYSQLUSER"
+host="DATABASEHOST"
+pass="MYSQLPASSWORD"
+systemuser="SYSTEMUSER"
+mysqldump -u"$dbuser" -h"$host" -p"$pass" --hex-blob --routines --triggers --all-databases | gzip > /home/"$systemuser"/MySQLBackups/monthly/"$db"_"$now".sql.gz
