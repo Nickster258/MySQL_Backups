e449dbec5f56b5a1e2ffd94d0829c811e696aae3
Nickster258 <nickster258@gmail.com>
2016-01-19 02:12:25 -0500

    Update mysqlbackup_monthly.sh

--- scripts/mysqlbackup_monthly.sh
+++ scripts/mysqlbackup_monthly.sh
@@ -5,4 +5,5 @@ dbuser="MYSQLUSER"
 host="DATABASEHOST"
 pass="MYSQLPASSWORD"
 systemuser="SYSTEMUSER"
-mysqldump -u"$dbuser" -h"$host" -p"$pass" --hex-blob --routines --triggers --all-databases | gzip > /home/"$systemuser"/MySQLBackups/monthly/"$db"_"$now".sql.gz
+dbdirectory="DIRECTORYLEADINGUPTOMYSQL_BACKUPSFOLDER"
+mysqldump -u"$dbuser" -h"$host" -p"$pass" --hex-blob --routines --triggers --all-databases | gzip > "$dbdirectory"/MySQL_Backups/monthly/"$db"_"$now".sql.gz
